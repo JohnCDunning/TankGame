9423a76563dd23565fcd261093ff495a75541f3e
remus.jones <remus.jones@hotmail.com>
2021-10-23 14:38:49 +1100

    Added Tank controls and deflected

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,13 +6,49 @@ using UnityEngine;
 public class PlayerController : Photon.MonoBehaviour
 {
     [SerializeField] private TankController _TankController;
-    
+    private Vector3 _LastPoint = Vector3.zero;
     private void Update()
     {
         if (base.photonView.isMine)
         {
             // get input
             _TankController.MoveTank(new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical")));
+            if (Input.GetMouseButtonDown(0))
+            {
+                _TankController.FireWeapon(_LastPoint);
+            }
+
+
+            if (Camera.main)
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        
+                if (Physics.Raycast(ray, out RaycastHit hit))
+                {
+                    _TankController.TurnTurret(hit.point);
+                    _LastPoint = hit.point;
+                }
+            }
+            else
+            {
+                Camera.SetupCurrent(GameObject.FindObjectOfType<Camera>());
+            }
         }
+    }
+
+    private void OnDrawGizmos()
+    {
+        
+        if (Camera.main)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                Gizmos.DrawSphere(hit.point, 0.2f);
+            }
+            
+        }
+
     }
 }
--- Assets/Project/Scripts/Core/TankController.cs
+++ Assets/Project/Scripts/Core/TankController.cs
@@ -1,29 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class TankController : MonoBehaviour
 {
     [SerializeField] private Rigidbody _Rigidbody;
-    [SerializeField] private float _MovementSpeed = 0f;
+    [SerializeField] private float _MovementSpeed = 1000f;
+    [SerializeField] private float _RotationSpeed = 100f;
+    [SerializeField] private Transform _Turret;
+    [SerializeField] private Transform _Body;
+    [SerializeField] private Transform _TurrentShootPoint;
     public void MoveTank(Vector3 direction)
     {
-        _Rigidbody.velocity = direction.normalized * _MovementSpeed;
-        TurnTank(direction.normalized);
+        _Rigidbody.velocity = _Body.forward * ((direction.z * _MovementSpeed)  * Time.deltaTime);
+
+        TurnTank(direction.x);
     }
 
-    public void TurnTank(Vector3 direction)
+    public void TurnTank(float turnDirection)
     {
-        this.transform.forward = direction;
+        _Body.transform.Rotate(new Vector3(0f, turnDirection * _RotationSpeed * Time.deltaTime, 0f), Space.World);
     }
 
-    public void TurnTurret(float lookAngle)
+    public void TurnTurret(Vector3 lookTarget)
     {
-        // look at mouse
+        Vector3 direction = lookTarget - transform.position;
+        float rotation = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+        _Turret.rotation = Quaternion.Euler(0, rotation, 0f);
     }
 
-    public void FireWeapon()
+    public void FireWeapon(Vector3 target)
     {
+        Vector3 turretPos = _TurrentShootPoint.position;
+        Vector3 adjustedTarget = new Vector3(target.x, turretPos.y, target.z);
+        Ray ray = new Ray(turretPos, adjustedTarget - turretPos);
         
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            Debug.DrawLine(turretPos, hit.point, Color.blue, 5f);
+
+            Ray bounceRay = new Ray(hit.point, hit.normal);
+            if (Physics.Raycast(bounceRay, out RaycastHit hitBounce))
+            {
+
+                Debug.DrawLine(hit.point, hitBounce.point * 100f, Color.red, 5f);
+            }
+        }
     }
 }

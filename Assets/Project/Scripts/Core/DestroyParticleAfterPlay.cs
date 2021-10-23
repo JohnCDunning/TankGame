9531b44f12215dbe29d951d07a11bfa66816a4f5
remus.jones <remus.jones@hotmail.com>
2021-10-23 22:07:03 +1100

    Added mine particles and functionality

---
+++ Assets/Project/Scripts/Core/DestroyParticleAfterPlay.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyParticleAfterPlay : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem _ParticleSystem;
+    private float _DestroyDelay = 0f;
+    void Start()
+    {
+        _DestroyDelay = _ParticleSystem.main.duration;
+    }
+
+    private IEnumerator LateDestroy(float time)
+    {
+        yield return new WaitForSeconds(time); 
+        Destroy(this.gameObject);
+    }
+}
